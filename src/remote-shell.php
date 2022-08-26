e2a3bf1b1ec2fb3a1b08ca7530d2902e1d81ebcd
me42th <david.meth@live.com>
2022-08-26 16:50:30 -0300

    init

--- src/remote-shell.php
+++ src/remote-shell.php
@@ -1,7 +1,10 @@
 <?php
 
 return [
+    // Server URI
     'uri' => 'http://localhost:8000',
-    'location' => 'http://localhost:8000/remote-shell',
-    'token' => ''
+    // Route on server
+    'route' => 'remote-shell',
+    // Put something here, needs be equals
+    'token' => '4pWy4pWt4pSB4pSB4pSB4pSB4pWu4pWy4pWyCuKVsuKUg+KVreKVruKVreKVruKUg+KVsuKVsgrilJfilKvilI/ilIHilIHilJPilKPilJvilbIK4pWy4pSD4pWw4pSB4pSB4pWv4pSDCuKVsuKVsOKUs+KUgeKUgeKUs+KVr+KVsuKVsg=='
 ];
