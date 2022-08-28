a51c8a8db3f3d862c2f6236f70433d8c5cddc693
me42th <david.meth@live.com>
2022-08-28 16:58:37 -0300

    [1] Auto install

--- src/Services/InstallService.php
+++ src/Services/InstallService.php
@@ -6,6 +6,6 @@ class InstallService{
         $path = __DIR__.'/config/remote-shell.php';
         $text = file_get_contents($path);
         $text = str_replace('############',md5(random_int(42,1000000000000)),$text);
-        file_put_contents($text,$path);
+        file_put_contents($path,$text);
     }
 }
